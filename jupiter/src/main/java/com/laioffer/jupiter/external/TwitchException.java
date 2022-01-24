f6edf0da687d2e3bed4ed1cd6fbf8ae9012b41e9
GHE37 <ghe37@gatech.edu>
2022-01-23 19:05:29 -0500

    updated 012322

---
+++ jupiter/src/main/java/com/laioffer/jupiter/external/TwitchException.java
@@ -0,0 +1,17 @@
+package com.laioffer.jupiter.external;
+
+
+
+public class TwitchException extends RuntimeException {
+    public TwitchException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+
+
+
+
+
+
+
